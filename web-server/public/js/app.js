4ff9e0904140d283cc9b8f19085675944ea99b81
Dan McKinney <3dhdaniel@gmail.com>
2020-03-25 17:58:38 -0400

    done for the day;

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -13,21 +13,27 @@ console.log('javascript wonders')
 // })
 
 const weatherForm = document.querySelector('form');
-const search = document.querySelector('input')
+const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1')
+const messageTwo = document.getElementById('message-2')
+
+// messageOne.textContent = 'From Javascript'
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-    console.log(location)
+    messageTwo.innerText = ''
+    messageOne.innerText = 'Loading...'
 
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
         if (data.error) {
-            console.log(data.error)
+            messageTwo.innerText = (data.error)
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
+           messageOne.textContent = (data.location)
+           messageTwo.textContent = (data.forecast)
         }
     })
     })
