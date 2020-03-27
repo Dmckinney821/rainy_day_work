e2e44b9b1808fe5ecb175a63b3eac479abb569bd
Dan McKinney <3dhdaniel@gmail.com>
2020-03-27 12:28:03 -0400

    im not deploying anythign else to heroki

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,8 @@
 
 const mongoose = require('mongoose');
+const validator = require('validator');
+
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
@@ -8,45 +11,74 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User',{
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('messd up a a ron')
+            }
+        },
+
     },
     age: {
         type: Number,
+        default: 0,
         validate(value) {
             if(value < 0) {
                 throw new Error('messd up')
             }
         }
+    },
+    password: {
+        type: String,
+        trim: true,
+        minlength: 87,
+        validate(value) {
+            if(value.toLowerCase().includes('password')){
+                throw new Error('try again')
+            }
+        }
     }
 })
 
-const me = new User({
-    name: 'Mickey',
-    age: 36
-})  
 
-me.save().then((me) => {
-    console.log(me)
-}).catch((error) => {
-    console.log('gone wrong', error)
-})
+// const me = new User({
+//     name: '   Mickey   ',
+//     email: 'DAN@gmail.com',
+//     password: '123abcd'
+// })  
+
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('gone wrong', error)
+// })
 
 const Task = mongoose.model('Task',{
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-// const task = new Task({
-//     description: 'Party',
-//     completed: false
-// })
+const task = new Task({
+    description: 'Party',
+    completed: false
+})
 
-// task.save().then((task) => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log(error)
-// })
+task.save().then((task) => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+})
